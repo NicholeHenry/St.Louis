f7ff0cfc591705ad88cab0cd14031c42ebf4ba27
Nichole Henry <nichole.henry12@gmail.com>
2019-07-09 23:55:18 -0500

    Validations in location index

--- St.Louis/ViewModels/Location/LocationCreateViewModel.cs
+++ St.Louis/ViewModels/Location/LocationCreateViewModel.cs
@@ -6,15 +6,19 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using St.Louis.Data;
 using St.Louis.Data.Repositories;
 using St.Louis.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace St.Louis.ViewModels.Location
 {
     public class LocationCreateViewModel
     {
-        
+        [Required (ErrorMessage ="Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter a description")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage ="Please create a description between 2- 200 words")]
         public string Description { get; set; }
         public string Address { get; set; }
+        [Required(ErrorMessage = "Please choose a region")]
         public Region Region { get; set; }
        // public int CategoryId { get; set; }
        // public string CategoryName { get; set; }
